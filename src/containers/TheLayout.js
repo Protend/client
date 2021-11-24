f3da102e4098c122fb7e10c49d0bc972f17644be
Geraldo Luiz <geraldobl58@gmail.com>
2021-11-24 19:44:35 -0300

    :sparkles: Login Verification

--- src/containers/TheLayout.js
+++ src/containers/TheLayout.js
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 import {
   TheContent,
   TheSidebar,
 } from './index'
 
+import useApi from '../services/api'
+
 const TheLayout = () => {
+  const api = useApi()
+  const history = useHistory()
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      if (api.getToken()) {
+        const result = await api.validateToken()
+        if (result.error === '') {
+          setLoading(false)
+        } else {
+          console.log(result.error)
+          history.push('/login')
+        }
+      } else {
+        history.push('/login')
+      }
+    }
+    checkLogin()
+  }, [api, history])
 
   return (
     <div className="c-app c-default-layout">
-      <TheSidebar/>
-      <div className="c-wrapper">
-        <div className="c-body">
-          <TheContent/>
-        </div>
-      </div>
+      {!loading && (
+        <>
+          <TheSidebar/>
+          <div className="c-wrapper">
+            <div className="c-body">
+              <TheContent/>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   )
 }
