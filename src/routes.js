bea4b3113435eb6d456b11192cd7b6bc0d1ce148
Geraldo Luiz <geraldobl58@gmail.com>
2021-12-01 10:07:35 -0300

    :sparkles: Finalizing unit crud

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ const Reservations = React.lazy(() => import('./views/Reservations'));
 const Warnings = React.lazy(() => import('./views/Warnings'));
 const FoundAndLost = React.lazy(() => import('./views/FoundAndLost'));
 const Users = React.lazy(() => import('./views/Users'));
+const Units = React.lazy(() => import('./views/Units'));
 const CommonAreas = React.lazy(() => import('./views/CommonAreas'));
 
 const routes = [
@@ -20,6 +21,7 @@ const routes = [
   { path: '/warnings', name: 'Warnings', component: Warnings },
   { path: '/foundandlost', name: 'FoundAndLost', component: FoundAndLost },
   { path: '/users', name: 'Users', component: Users },
+  { path: '/units', name: 'Units', component: Units },
   { path: '/commonareas', name: 'CommonAreas', component: CommonAreas },
 ];
 
